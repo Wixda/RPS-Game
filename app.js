05afd63d1e1d56823d934cdcdca5f03c6323928e
Wixda <Iamwixda@gmail.com>
2025-10-30 18:06:16 +0100

    Write the logic to get the human choice

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function getComputerChoice() {
-    let randomNO = Math.ceil(Math.random() * 3);
-    if (randomNO === 1) {
+    let computerNO = Math.ceil(Math.random() * 3);
+    if (computerNO === 1) {
         return "Rock";
-    } else if (randomNO === 2) {
+    } else if (computerNO === 2) {
         return "Paper";
-    } else if (randomNO === 3) {
+    } else if (computerNO === 3) {
+        return "Scissors";
+    } else return null;
+}
+
+function getHumanChoice() {
+    let humanNo = Number(prompt("Enter a number between 1 to 3"))
+    if (humanNo === 1) {
+        return "Rock";
+    } else if (humanNo === 2) {
+        return "Paper";
+    } else if (humanNo === 3) {
         return "Scissors";
     } else return null;
 }
